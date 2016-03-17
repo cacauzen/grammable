52ac6a7a9c40551b5f6e813056d82568b122b81a
Claudia Almeida <ihavenids@gmail.com>
2016-03-17 16:12:01 +0000

    Added profile picture, first name and last name fields to registrations to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-
+  mount_uploader :profile_picture, PictureUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,4 +7,17 @@ class User < ActiveRecord::Base
 
   has_many :grams
   has_many :comments
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password,
+      :password_confirmation, :remember_me, :profile_picture, :picture_cache) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password,
+      :password_confirmation, :current_password, :profile_picture, :picture_cache) }
+  end
+
+  #validates_presence_of   :picture
+  #validates_integrity_of  :avatar
+  #validates_processing_of :avatar
 end
